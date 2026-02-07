0ff5dddf275bc6b6a9cfd126ccedb2bfba10a98f
maria <135614312+garciaqueen@users.noreply.github.com>
2026-02-07 11:10:57 +0200

    Task 11.2. Corrected. Refactor Vector functions to use pointer syntax
    Zdefiniuj strukturę Vector, która będzie realizowała podstawową funkcjonalność dynamicznej tablicy liczb całkowitych typu int. W zdefiniuj funkcje:
    - init() - inicjującą elementy składowe obiektu,
    - int& at(int i) - zwracającą referencję na i-ty element tablicy,
    - void append(int v) - dodającą element na koniec tablicy,
    - void resize(int size) - zmieniającą rozmiar tablicy na nowy, zachowując elementy już dodane do tablicy,
    - int size() - zwracającą rozmiar tablicy,
    - void clear() - czyszczącą całą zawartość obiektu.
     Napisz program, w którym przetestujesz implementację tej klasy.

--- struktury/zadanie11.cpp
+++ struktury/zadanie11.cpp
@@ -11,39 +11,52 @@ struct Vector {
     int len;
 };
 
-void init(Vector& v) {
-    v.data = nullptr;
-    v.len = 0;
+void init(Vector* v) {
+    v->data = nullptr;
+    v->len = 0;
 }
 
-int& at(Vector &a, int i) {
+int& at(Vector& a, int i) {
     return a.data[i];
 }
 
-void resize(Vector& v,int size) {
-    v.len = size;
+void resize(Vector* v, int size) {
+    int* new_data = new int[size];
+    for (int i = 0; i < v->len; i++) {
+        new_data[i] = v->data[i];
+    }
+    delete[] v->data;
+    v->data = new_data;
+    v->len = size;
+    
 }
 
-void append(Vector & a, int v) {
-    resize(a, a.len+1);
-    a.data[a.len + 1] = v;
+void append(Vector* v, int value) {
+    // add a new alement at the end
+    // we have a table with a certain amount of elements
+    // we resize and add the element at the end
+    resize(v, (v->len + 1));
+    v->data[v->len-1] = value;
 }
 
 int size(Vector& a) {
     return a.len;
 }
 
-void clear(Vector &a) {
-    delete[] a.data;
-    a.data = nullptr;
-    a.len = 0;
+void clear(Vector* a) {
+    delete[] a->data;
+    a->data = nullptr;
+    a->len = 0;
 }
 
 int main()
 {
-    Vector v;
+    Vector* v = new Vector;
     init(v);
-
+    append(v, 5);
+    append(v, 2);
+    append(v, 7);
+    cout << v->data[2];
     return 0;
 
 }
