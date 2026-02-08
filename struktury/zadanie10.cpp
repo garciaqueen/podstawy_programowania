e77dff24b3ddd91cf701675ebeab542f77f7c5e7
maria <135614312+garciaqueen@users.noreply.github.com>
2026-02-08 17:30:04 +0200

    Refactor Task 10 structure and update functions
    Refactor Task structure and sorting functions. Update task name handling and modify print function.

--- struktury/zadanie10.cpp
+++ struktury/zadanie10.cpp
@@ -1,82 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
-#include <cmath>
-#include <cstdlib>
-#include <algorithm>
+#include <cstdio>
 #include <cstring>
+#include <algorithm>
 
 using namespace std;
 
 struct Task {
-    char name[30];
+    char task_name[50];
     int prior;
     void (*run)(const char*, int);
-
 };
 
-
-void print(const char* name, int prior) {
-    cout << name << " " << prior << '\n';
+void print(const char* str, int num) {
+    cout << str << ' ' << num << endl;
 }
 
-bool sort_prior(Task *a, Task *b) {
-    return a->prior < b->prior;
+bool sort_p(Task* a1, Task* a2) {
+    return a1->prior < a2->prior;
 }
 
-bool sort_char(Task * a, Task * b) {
-    return strcmp(a->name, b->name) > 0;
+bool sort_name(Task* a1, Task* a2) {
+    return strcmp(a1->task_name, a2->task_name) == 1;
 }
 
-void plus_prior(Task *a) {
+void one_more(Task* a) {
     a->prior += 1;
-}
-
-void print_r(const Task* a) {
-    a->run(a->name, a->prior);
 }
 
 int main()
 {
     Task** tab = new Task * [3];
     tab[0] = new Task;
-    tab[0]->name[0] = 'T';
-    tab[0]->name[1] = 'a';
-    tab[0]->name[2] = 's';
-    tab[0]->name[3] = 'k';
-    tab[0]->name[4] = '1';
-    tab[0]->name[5] = '\0';
-    tab[0]->prior = 3;
+    strcpy(tab[0]->task_name, "Tasa1");
+    tab[0]->prior = 4;
     tab[0]->run = print;
     tab[1] = new Task;
-    tab[1]->name[0] = 'T';
-    tab[1]->name[1] = 'a';
-    tab[1]->name[2] = 's';
-    tab[1]->name[3] = 'k';
-    tab[1]->name[4] = '2';
-    tab[1]->name[5] = '\0';
-    tab[1]->prior = 1;
+    strcpy(tab[1]->task_name, "Tasz2");
+    tab[1]->prior = 7;
     tab[1]->run = print;
     tab[2] = new Task;
-    tab[2]->name[0] = 'T';
-    tab[2]->name[1] = 'a';
-    tab[2]->name[2] = 's';
-    tab[2]->name[3] = 'k';
-    tab[2]->name[4] = '3';
-    tab[2]->name[5] = '\0';
+    strcpy(tab[2]->task_name, "Task3");
     tab[2]->prior = 2;
     tab[2]->run = print;
 
-    cout << "Sort prior: ";
-    std::sort(tab, tab + 3, sort_prior);
-    cout << "Sort char: ";
-    std::sort(tab, tab + 3, sort_char);
-    cout << "Plus prior: ";
-    std::for_each(tab, tab + 3, plus_prior);
-    cout << "Print r: ";
-    std::for_each(tab, tab + 3, print_r);
+    sort(tab, tab + 3, sort_p);
 
+
+    sort(tab, tab + 3, sort_name);
+
+    for_each(tab, tab + 3, one_more);
+
+    tab[0]->run(tab[0]->task_name, tab[0]->prior);
 
     return 0;
-
-}
 
 }
